e4b31ff111fabe34410ab1f29eb32e4cd64be72a
Atirek Pothiwala <knight.developer9@gmail.com>
2026-09-24 19:44:06 +0000

    Set app display name to Brew Coffee
    Add AppBranding constants and wire platform titles (web, Android, iOS)
    and MaterialApp title to the Brew Coffee name.

---
+++ android/app/src/main/kotlin/com/brewcoffee/brew_coffee/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.brewcoffee.brew_coffee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/constants/app_branding.dart
@@ -0,0 +1,12 @@
+/// Central branding strings for Brew Coffee.
+abstract final class AppBranding {
+  static const String appName = 'Brew Coffee';
+
+  /// Short mark used on receipts and compact headers.
+  static const String logoMark = 'BREW';
+
+  static const String tagline = 'Your coffee. Your way.';
+
+  /// Secondary line on printed-style receipts.
+  static const String receiptShopLine = 'BREW COFFEE';
+}
