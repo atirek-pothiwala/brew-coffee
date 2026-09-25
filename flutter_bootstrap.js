6bd48b40649557003b45c031928c92a358777782
Atirek Pothiwala <knight.developer9@gmail.com>
2026-09-25 20:45:10 +0000

    Deploy 2026-09-25T20:45:10Z

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2314766351"
+    serviceWorkerVersion: "1775561821"
   }
 });
